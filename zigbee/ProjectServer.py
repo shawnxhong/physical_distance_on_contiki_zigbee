e84808246278c4a7324292504d027004b23383fe
Wanchen Zhao <iamclausius@gmail.com>
2020-11-20 19:19:14 +1000

    update server

--- zigbee/ProjectServer.py
+++ zigbee/ProjectServer.py
@@ -8,29 +8,27 @@ import StringIO
 from threading import Thread
 import sys
 
-UDP_TIMESYNC_PORT = 3000 # node listens for timesync packets on port 4003
-UDP_REPLY_PORT = 3001 # node listens for reply packets on port 7005
-FILE = "record.txt"
-
+UDP_SEND_PORT = 3000 # port for sending packets
+UDP_REPLY_PORT = 3001 # node listens for reply packets
+FILE = "record.csv"
 
 isRunning = True
 
-
 def udpListenThread():
- # listen on UDP socket port UDP_TIMESYNC_PORT
+ # listen on UDP socket port UDP_SEND_PORT
   recvSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
   recvSocket.bind(("aaaa::1", UDP_REPLY_PORT))
   recvSocket.settimeout(0.5)
 
   while isRunning:
     try:
-	data, addr = recvSocket.recvfrom( 1024 )
-	#timestamp = (struct.unpack("I", data[0:4]))[0]
-	print "Reply from:", addr[0], "Data", data
-	f = open(FILE, "a+")
-	f.write(data+"\r\n")
-	f.close()
-
+    	data, addr = recvSocket.recvfrom( 1024 )
+    	print "Reply from:", addr[0], "Data:", data
+      # print "Reply from:", addr[0], "UTC[s]:", data, "Localtime:", utc.strftime("%Y-%m-%d %H:%M:%S")
+    	f = open(FILE, "a+")
+    	f.write(data+"\r\n")
+    	f.close()
+    
     except socket.timeout:
       pass
     
@@ -40,24 +38,21 @@ def udpSendThread():
 
   while isRunning:
     timestamp = int(time.time())
-#    print "Sending timesync packet with UTC[s]:", timestamp, "Localtime:", time.strftime("%Y-%m-%d %H:%M:%S")
-
+    print "Sending", timestamp
     # send UDP packet to nodes
     # change the IP Address with your sensorTag accordingly.
     # you may start with one sensortag.
-    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1205:2bcb", UDP_TIMESYNC_PORT))
-    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1665:2280", UDP_TIMESYNC_PORT))
-    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1665:b601", UDP_TIMESYNC_PORT))
-#    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:f18:e384", UDP_TIMESYNC_PORT))
-#    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1205:2aa1", UDP_TIMESYNC_PORT))
+    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1205:2bcb", UDP_SEND_PORT))
+    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1665:2280", UDP_SEND_PORT))
+    sock.sendto(struct.pack("I", timestamp), ("aaaa::212:4b00:1665:b601", UDP_SEND_PORT))
   
     # sleep for some seconds
     # the frequency of sending the sych timestamps packet is very important
     # you will see how this affect the sych accuracy in your experiment
-    time.sleep(1)
+    time.sleep(2)
 
 f = open(FILE, "w+")
-f.write("Record\r\n")
+f.write("Contact\r\n")
 f.close()
 
 # start UDP listener as a thread
@@ -71,7 +66,7 @@ time.sleep(1)
 t2 = Thread(target=udpSendThread)
 t2.start()
 
-#print "Sending timesync packets on UDP port", UDP_TIMESYNC_PORT
+#print "Sending timesync packets on UDP port", UDP_SEND_PORT
 print "Exit application by pressing (CTRL-C)"
 
 try:
@@ -81,5 +76,7 @@ try:
 except KeyboardInterrupt:
   print "Keyboard interrupt received. Exiting."
   isRunning = False
+
+
 
 
